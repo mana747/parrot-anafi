2d858421f43dc157b72737f710b8fbeb4fc2b1d7
mana747 <87841184+mana747@users.noreply.github.com>
2022-06-16 15:49:50 +0900

    Add files via upload

--- moveby.py
+++ moveby.py
@@ -1,10 +1,8 @@
 # -*- coding: UTF-8 -*-
 #!/usr/bin/python3
-
 import olympe
 from olympe.messages.ardrone3.Piloting import TakeOff, moveBy, Landing
 from olympe.messages.ardrone3.PilotingState import FlyingStateChanged
-
 DRONE_IP = "192.168.42.1"
 
 if __name__ == "__main__":
@@ -15,8 +13,11 @@ if __name__ == "__main__":
         >> FlyingStateChanged(state="hovering", _timeout=5)
     ).wait().success()
     assert drone(
-        moveBy(2, 1, -0.5, 3.14)
+        moveBy(1, 0, 0, 0)
         >> FlyingStateChanged(state="hovering", _timeout=5)
     ).wait().success()
     assert drone(Landing()).wait().success()
     drone.disconnect()
+
+
+
