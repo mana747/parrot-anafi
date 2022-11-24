7d32cc67c4f7145bb7ab608e44ac7c89136a82fd
mana747 <87841184+mana747@users.noreply.github.com>
2022-11-24 16:11:29 +0900

    Update kato.py

--- kato.py
+++ kato.py
@@ -49,3 +49,32 @@ if __name__ == "__main__" :
             time.sleep(0.5)
     except KeyboardInterrupt:
         cam0_mark_search.cap.release()
+        
+### マーカーIDと、動画を重ねて表示する
+import cv2
+from cv2 import aruco
+import time
+
+### --- aruco設定 --- ###
+dict_aruco = aruco.Dictionary_get(aruco.DICT_4X4_50)
+parameters = aruco.DetectorParameters_create()
+
+RTSP_URL ='rtsp://192.168.42.1/live'
+cap = cv2.VideoCapture(RTSP_URL)
+
+try:
+    while True:
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, dict_aruco, parameters=parameters)
+
+        frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+        cv2.imshow('frame', frame_markers)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cv2.destroyWindow('frame')
+    cap.release()
+except KeyboardInterrupt:
+    cv2.destroyWindow('frame')
+    cap.release()
